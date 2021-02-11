b4ab4858418fcad67a6e52963e0b9ea5d859e6ea
Agata Gołębiowska <agataxgolebiowska@gmail.com>
2021-02-11 13:10:23 +0100

    Init django project & app

---
+++ backend/todo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class TasksList(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    task_list = models.ForeignKey(TasksList, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    is_done = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.description
